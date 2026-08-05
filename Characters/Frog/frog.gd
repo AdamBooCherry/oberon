c88223186ccbb240848846efaa0637a769a34a3a
AdamBooCherry <bucceriadam@gmail.com>
2026-08-04 20:48:48 -0600

    Post processing and also a plugin

---
+++ Characters/Frog/frog.gd
@@ -0,0 +1,15 @@
+extends CharacterBody3D
+class_name FrogMob
+
+## hide
+## run away
+## wander
+## invisible / reveal
+## stun
+## catch
+## gets added to inventory/currency
+
+
+
+func get_picked_up():
+	pass
