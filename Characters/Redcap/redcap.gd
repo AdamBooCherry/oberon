f220197a6d7b1f68042110e50983b237e15c3480
AdamBooCherry <bucceriadam@gmail.com>
2026-08-25 14:23:44 -0600

    currency counter

---
+++ Characters/Redcap/redcap.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+class_name Redcap
+
+### THIS GUY IS A MGS 1 GUARD
+### Searches for player
+### Pursues player
+### Attacks player
+### Can be stunned, which sets him back to searching
+
+## STATES
+### IDLE
+### WANDER
+### SEARCHING
+### ALERT
+### HUNTING
+### ATTACKING
+### STUNNED
+### DISTRACTED
+
+## EMIT CUTSCENE SIGNAL IF HE KILLS YOU
