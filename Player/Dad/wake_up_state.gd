f8574aa743e12529469165fb4d40237d6689439d
AdamBooCherry <bucceriadam@gmail.com>
2026-08-29 12:44:44 -0600

    wake up faster

--- Player/Dad/wake_up_state.gd
+++ Player/Dad/wake_up_state.gd
@@ -10,15 +10,13 @@ func enter() -> void:
 	player_dad.disable_action_control()
 	
 	print("STATE: DadWakeupState -> Playing stand_up animation...")
-	player_dad.animation_player.play("Dad/stand_up")
+	player_dad.animation_player.play("Dad/stand_up", -1, 2.5)
 	await player_dad.animation_player.animation_finished
-	print("STATE: DadWakeupState -> stand_up animation finished.")
 	
 	if control_delay > 0.0:
 		print("STATE: DadWakeupState -> Waiting control delay (", control_delay, "s)...")
 		await player_dad.get_tree().create_timer(control_delay).timeout
 		
-	print("STATE: DadWakeupState -> Re-enabling hurtbox and controls.")
 	if player_dad.hurtbox:
 		player_dad.hurtbox.set_deferred("monitoring", true)
 		player_dad.hurtbox.set_deferred("monitorable", true)
